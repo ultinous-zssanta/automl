0ab82df93782bb9ba2c3cbabf17edd1f08dd52ad
Zsolt Santa <zssanta@ultinous.com>
2020-04-01 10:14:49 +0000

    improve argument handling for train.sh

--- efficientdet/train.sh
+++ efficientdet/train.sh
@@ -1,13 +1,33 @@
 #! /bin/bash
 
+set -e
+set -o pipefail
+
+SCRIPT_PATH=$( dirname `realpath $0` )
 TRAIN_DATASET_ROOT="/home/zssanta/fastdata/COCO/tfrecords/train2017"
 MODEL="efficientdet-d0"
-TRAIN_OUTPUT="/tmp/${MODEL}"
+TRAIN_OUTPUT="${PWD}/training/${MODEL}"
+LOG_NAME=${TRAIN_OUTPUT}/train.log
 
-python main.py \ 
-  --training_file_pattern="${TRAIN_DATASET_ROOT}/train*" \
-  --model_name=${MODEL} \
-  --model_dir=${TRAIN_OUTPUT} \
-  --hparams="use_bfloat16=false" \
+[[ ! -d ${TRAIN_OUTPUT} ]] && mkdir -p ${TRAIN_OUTPUT}
+
+ARGS=(
+  --training_file_pattern="${TRAIN_DATASET_ROOT}/train*"
+  --model_name=${MODEL}
+  --model_dir=${TRAIN_OUTPUT}
+  --hparams="use_bfloat16=false"
   --use_tpu=False
+  --train_batch_size=10
+)
+
+savelog -nl ${LOG_NAME}
+python ${SCRIPT_PATH}/main.py "${ARGS[@]}" 2>&1 | tee ${LOG_NAME}
+
+# python main.py --training_file_pattern="${TRAIN_DATASET_ROOT}/train*" \
+#   --model_name=${MODEL} \
+#   --model_dir=${TRAIN_OUTPUT} \
+#   --hparams="use_bfloat16=false" \
+#   --use_tpu=False \
+#   --train_batch_size=16 \
+#   2>&1 | tee ${LOG_NAME}
 
